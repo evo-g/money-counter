05b4663d4b7c3fdc77f7fbc91dbe37adc0a00d7a
Evodio Garcia <evodiog@gmail.com>
2020-09-21 18:12:07 -0700

    creates inital files from a codepen I made, needs styling and some extra edge cases for logic

---
+++ README.md
@@ -0,0 +1,8 @@
+# Money Counter
+
+## Made to help count small or large amounts of money to do bank deposits 
+- Basic JavaScript, HTML, CSS, and Parcel.js
+
+```
+npm i
+```
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const hundred = document.getElementById('hundred');
+const fifty = document.getElementById('fifty');
+const twenty = document.getElementById('twenty');
+const ten = document.getElementById('ten');
+const five = document.getElementById('five');
+const one = document.getElementById('one');
+const totals = document.getElementById('totals');
+const totalsAmount = document.getElementById('totalsAmount');
+const valuesArr = [100,50,20,10,5,1];
+const inputValsArr = [hundred, fifty, twenty, ten, five, one];
+
+function displayTotals(arr = []) {
+  totals.innerHTML =
+    arr.map(el => {
+      return `<li class='dollar-amounts'> ${el[0]}'s: ${el[1]}</li>`;
+    }).join('');
+};
+
+function getTotals(e) {
+  e.preventDefault();
+  let computedTotals = valuesArr.map((el, i) => {
+    return [valuesArr[i], inputValsArr[i].value * el]
+  });
+  let combinedTotal = 0;
+  valuesArr.forEach((val, i) => {
+    combinedTotal += inputValsArr[i].value * val;
+  })
+  console.log(combinedTotal);
+  displayTotals(computedTotals);
+  totalsAmount.innerText = `Grand total ${combinedTotal}`;
+};
+
+document.addEventListener('submit', getTotals);
