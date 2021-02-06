75d56895c52701ddc94bbf76733dd7e3b31cf08c
Evodio Garcia <evodiog@gmail.com>
2021-02-06 02:50:37 -0800

    css changes and minor functionality imporvements to displayed totals and form

--- src/index.js
+++ src/index.js
@@ -6,20 +6,23 @@ const five = document.getElementById('five');
 const one = document.getElementById('one');
 const totals = document.getElementById('totals');
 const totalsAmount = document.getElementById('totalsAmount');
-const valuesArr = [100,50,20,10,5,1];
+const valuesArr = [100, 50, 20, 10, 5, 1];
 const inputValsArr = [hundred, fifty, twenty, ten, five, one];
 
 function displayTotals(arr = []) {
   totals.innerHTML =
     arr.map(el => {
-      return `<li class='dollar-amounts'> ${el[0]}'s: -- $ ${el[1]}</li>`;
+      if (!el[1]) {
+        el[1] = 0
+      };
+      return `<div class='dollar-amounts'> $${el[0]} x ${el[1]} = $ ${el[2]}</div>`;
     }).join('');
 };
 
 function getTotals(e) {
   e.preventDefault();
-  let computedTotals = valuesArr.map((el, i) => {
-    return [valuesArr[i], inputValsArr[i].value * el]
+  let computedTotals = valuesArr.map((el, i,) => {
+    return [valuesArr[i], inputValsArr[i].value, inputValsArr[i].value * el]
   });
   let combinedTotal = 0;
   valuesArr.forEach((val, i) => {
@@ -29,4 +32,9 @@ function getTotals(e) {
   totalsAmount.innerText = `Combined total $${combinedTotal}`;
 };
 
+function resetForm() {
+  document.querySelector('form').reset();
+};
+
 document.addEventListener('submit', getTotals);
+document.addEventListener('reset', resetForm);
